4a1e0784b8c577309798f66956a1adf518bbbb0d
Nitaa1904 <nitafitrotul1904@gmail.com>
2024-09-29 08:54:55 +0700

    update index.js

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 const fs = require("fs").promises;
 const fsSync = require("fs")
-
-// 1. membaca file surat cinta fsw 2
+// SYNC
+// 1. membaca file surat cinta fsw 2 dari index.txt
 let loveLetter = fsSync.readFileSync("./index.txt", "utf-8")
-
-// 2. proses print isi dari surat cinta
+// 2. proses print dari surat 
 console.log(`ini baris 7: ${loveLetter}`) 
 
 // 3. membuat file baru untuk balas surat cinta
-const loveFeedback = "Aku juga sayang FSW 2!"
+// defind variabel untuk balasan
+const loveFeedback = "Aku juga sayang FSW 2!!!"
 fsSync.writeFileSync("./balasan.txt", loveFeedback)
+// console.log(writeResult)
+// membuat folder
+// fs.mkdir("COBA_BUAT_FOLDER", () => {})
 
-// fs.mkdir("COBA_BIKIN_FOLDER_2", () => {})
 
-// menimpa isi konten dari index.txt
+// ASYNC
+// asynchronous file/write
 fsSync.writeFileSync("./index.txt", "KETIMPA GAK CINTA KITA PART FINAL SEBELUM BREAK PART 2?")
 
 async function bacaSuratCinta() {
@@ -37,4 +40,4 @@ fs.readFile("./index.txt", "utf-8")
         console.error("Error occured: ", error)
     })
 
-console.log(`ini baris 21 ${loveLetter}`)
+console.log(`ini baris 43 ${loveLetter}`)
